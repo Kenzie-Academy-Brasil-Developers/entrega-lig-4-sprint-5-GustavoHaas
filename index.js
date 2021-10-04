a55ec2c15b2e8f54982929bd9b359f3f78429560
Débora Gama <gama.debora1@gmail.com>
2021-10-04 15:48:49 -0300

    Consttruindo array e criando board no DOM

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+const boardArr = [
+    "C,C,C,C,C,C,C",
+    "C,C,C,C,C,C,C",
+    "C,C,C,C,C,C,C",
+    "C,C,C,C,C,C,C",
+    "C,C,C,C,C,C,C",
+    "C,C,C,C,C,C,C",
+];
 
+
+
+const board = document.getElementById("board");
+
+
+
+let line = "";
+let cell = "";
+function buildingBoard(){
+
+    
+    for(let l = 0; l < boardArr.length; l++){
+        line = boardArr[l];
+      
+        const lineDiv = document.createElement("div");
+        lineDiv.classList.add("line");
+        lineDiv.id = "line" + (l)
+        board.appendChild(lineDiv);
+
+        for(let c = 0; c < line.length; c ++){
+            cell = line[c];
+            const cellDiv = document.createElement("div");
+            cellDiv.classList.add("cell");
+            cellDiv.id = "cell" + "c"
+            lineDiv.appendChild(cellDiv)
+
+        }
+
+    }
+
+
+}
+buildingBoard()
